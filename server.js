9006e87c2318a30cd7f0b239a3fc302831a36005
Rohan Maharjan <smdnoobs24@gmail.com>
2021-03-23 17:45:07 +0545

    cors fix

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
+
+const whitelist = ['https://od-api.oxforddictionaries.com/api/v2'];
+
+const corsOptions = {
+  origin: function(origin, callback) {
+    if(!origin){
+      return callback(null, true);
+    }
+
+    if(whitelist.indexOf(origin) === -1){
+      var msg = 'The CORS policy for this site does not allow access from the specified origin.';
+      return callback(new Error(msg), false);
+    }
+    return callback(null, true);
+  }
+}
+
+app.use(cors(corsOptions));
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/dist/DictionaryApi'));
